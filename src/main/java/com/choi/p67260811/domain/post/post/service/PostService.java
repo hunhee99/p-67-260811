44321f917464ca3b5a3e05c62e231f17a5ef037a
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-08-12 09:44:55 +0900

    DTO 적용

---
+++ src/main/java/com/choi/p67260811/domain/post/post/service/PostService.java
@@ -0,0 +1,50 @@
+package com.choi.p67260811.domain.post.post.service;
+
+
+import com.choi.p67260811.domain.post.comment.entity.PostComment;
+import com.choi.p67260811.domain.post.post.entity.Post;
+import com.choi.p67260811.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post write(String title, String content) {
+        Post post = new Post(title, content);
+        return postRepository.save(post);
+    }
+
+    public PostComment writeComment(Post post, String content) {
+        return post.addComment(content);
+    }
+
+    public void deleteComment(Post post, int commentId) {
+        post.removeComment(commentId);
+    }
+
+    public PostComment modifyComment(Post post, int commentId, String content) {
+        return post.modifyComment(commentId, content);
+    }
+
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public long count() {
+        return postRepository.count();
+    }
+    public void flush() {
+        postRepository.flush();
+    }
+}
